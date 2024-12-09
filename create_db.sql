758f11fb6f1d0486f970828e7dbec21391280ed9
OMesk <omesk001@doc.gold.ac.uk>
2024-12-09 13:50:38 +0000

    database is set up in the mos tbasic way without data

--- create_db.sql
+++ create_db.sql
@@ -1,12 +1,31 @@
-# Create database script for Bettys books
+CREATE DATABASE  IF NOT EXISTS `os-portfolio` 
+USE `os-portfolio`;
+DROP TABLE IF EXISTS projects;
+CREATE TABLE projects (
+  idProjects int NOT NULL,
+  projectName varchar(45) NOT NULL,
+  GitLink varchar(500) NOT NULL,
+  PRIMARY KEY (idProjects),
+  UNIQUE KEY idProjects_UNIQUE (idProjects),
+  UNIQUE KEY projectName_UNIQUE (projectName),
+  UNIQUE KEY GitLink_UNIQUE (GitLink)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-# Create the database
-CREATE DATABASE IF NOT EXISTS Os-portfolio;
-USE Os-portfolio;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  idusers int NOT NULL,
+  userName varchar(45) DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (idusers)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-# Create the tables
-CREATE TABLE IF NOT EXISTS projects (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+DROP TABLE IF EXISTS views;
 
-# Create the app user
-CREATE USER IF NOT EXISTS 'Os-portfolio_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
-GRANT ALL PRIVILEGES ON Os-portfolio.* TO ' Os-portfolio_app'@'localhost';
+CREATE TABLE views (
+  userID int NOT NULL,
+  projectID int NOT NULL,
+  KEY FKuserID_idx (userID),
+  KEY FKprojectID_idx (projectID),
+  CONSTRAINT FKprojectID FOREIGN KEY (projectID) REFERENCES projects (idProjects),
+  CONSTRAINT FKuserID FOREIGN KEY (userID) REFERENCES users (idusers)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
