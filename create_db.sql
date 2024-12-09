7a9d4966ad6ce2914553658881a36abbbbcc30fb
OMesk <omesk001@doc.gold.ac.uk>
2024-12-09 17:22:40 +0000

    Database matches registry page, along with dummy data and room for testing

--- create_db.sql
+++ create_db.sql
@@ -1,22 +1,25 @@
 CREATE DATABASE  IF NOT EXISTS `os-portfolio` 
 USE `os-portfolio`;
 DROP TABLE IF EXISTS projects;
-CREATE TABLE projects (
-  idProjects int NOT NULL,
-  projectName varchar(45) NOT NULL,
-  GitLink varchar(500) NOT NULL,
-  PRIMARY KEY (idProjects),
-  UNIQUE KEY idProjects_UNIQUE (idProjects),
-  UNIQUE KEY projectName_UNIQUE (projectName),
-  UNIQUE KEY GitLink_UNIQUE (GitLink)
+CREATE TABLE `projects` (
+  `idProjects` int NOT NULL AUTO_INCREMENT,
+  `projectName` varchar(45) NOT NULL,
+  `GitLink` varchar(500) NOT NULL,
+  PRIMARY KEY (`idProjects`),
+  UNIQUE KEY `idProjects_UNIQUE` (`idProjects`),
+  UNIQUE KEY `projectName_UNIQUE` (`projectName`),
+  UNIQUE KEY `GitLink_UNIQUE` (`GitLink`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  idusers int NOT NULL,
-  userName varchar(45) DEFAULT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (idusers)
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `idusers` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(45) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `email` varchar(450) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`idusers`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS views;
--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,6 +1,27 @@
 # Insert data into the tables
+-- Clear all data from tables
+DELETE FROM views;
+DELETE FROM users;
+DELETE FROM projects;
 
-USE Os-portfolio;
+-- Reset auto-increment values (optional, for consistency)
+ALTER TABLE views AUTO_INCREMENT = 1;
+ALTER TABLE users AUTO_INCREMENT = 1;
+ALTER TABLE projects AUTO_INCREMENT = 1;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+-- Insert test data into projects
+INSERT INTO projects (idProjects, projectName, GitLink) VALUES
+(1, 'SecCo', 'https://github.com/OrtvinM/Sec-Co-Website.git'),
+
+
+-- Insert test data into users
+INSERT INTO users (idusers, userName, `password`) VALUES
+(1,'Om','Ortvin','M','ortvin14@gmail.com','F1bb2Kv@lmnOqF'),
+(2,'SBwilder', 'Stevn','Backs','SB@gmail.com', 'password'),
+(3,'MrNightmare','worse','User','Wronguser@yahoo.com','123');
+
+-- Insert test data into views
+INSERT INTO views (userID, projectID) VALUES
+(1, 1), --  Ortvin M visits Sec-Co
+(1, 2), --  Steven Backs visits Sec-Co
 
