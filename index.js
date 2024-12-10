bc92c6fcc2c108a923732be0b0e09573dee36bd7
OMesk <omesk001@doc.gold.ac.uk>
2024-12-10 20:44:59 +0000

    user list + database test data adjustments

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 // Import express and ejs
-var express = require ('express')
-var ejs = require('ejs')
+const express = require('express');
+const ejs = require('ejs');
 
-//Import mysql module
-var mysql = require('mysql2')
-
+// Import mysql2 module
+const mysql = require('mysql2');
 
 // Create the express application object
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 // Tell Express that we want to use EJS as the templating engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-// Set up the body parser 
-app.use(express.urlencoded({ extended: true }))
+// Set up the body parser
+app.use(express.urlencoded({ extended: true }));
 
-// Set up public folder (for css and statis js)
-app.use(express.static(__dirname + '/public'))
+// Set up public folder (for CSS and static JS)
+app.use(express.static(__dirname + '/public'));
 
 // Define the database connection
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
     host: 'localhost',
-    user: 'Os-portfolio_app',
+    user: 'os-portfolio_app',
     password: 'qwertyuiop',
-    database: 'Os-portfolio'
-})
-// Connect to the database
+    database: 'os-portfolio'
+});
+
+// Connect to the database 
 db.connect((err) => {
     if (err) {
-        throw err
+        console.error('Error connecting to the database:', err.message);
+        console.error('Ensure the MySQL credentials are correct and the MySQL service is running.');
+        process.exit(1); 
+    } else {
+        console.log('Connected to database');
     }
-    console.log('Connected to database')
-})
-global.db = db
-
+});
+global.db = db;
 // Define our application-specific data
-app.locals.siteData = {siteName: "Bettys Books"}
-
+app.locals.siteData = { siteName: "Sec Co - Ortvin M Portfolio project" };
 // Load the route handlers
-const mainRoutes = require("./routes/main")
-app.use('/', mainRoutes)
-
+const mainRoutes = require("./routes/main");
+app.use('/', mainRoutes);
 // Load the route handlers for /users
-const usersRoutes = require('./routes/users')
-app.use('/users', usersRoutes)
-
-// Load the route handlers for /books
-const booksRoutes = require('./routes/books')
-app.use('/books', booksRoutes)
-
+const usersRoutes = require('./routes/users');
+app.use('/users', usersRoutes);
+// Load the route handlers for /projects
+const projectsRoutes = require('./routes/projects');
+app.use('/projects', projectsRoutes);
 // Start the web app listening
-app.listen(port, () => console.log(`Node app listening on port ${port}!`))
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
