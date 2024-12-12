d75b85f4db3d9efb49e27479836ab70a18759538
OMesk <omesk001@doc.gold.ac.uk>
2024-12-12 15:06:08 +0000

    styling changes, as well as an github api addiction

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// Import express and ejs
 const express = require('express');
 const ejs = require('ejs');
-
-// Import mysql2 module
 const mysql = require('mysql2');
-
-// Import session module
 const session = require('express-session');
 
 // Create the express application object
 const app = express();
 const port = 8000;
 
-const expressSanitizer = require('express-sanitizer');
-var validator = require ('express-validator');
-
-app.use(expressSanitizer());
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
 
@@ -27,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Create a session
 app.use(session({
-    secret: 'BackwardsDucksGoThirteenMaybeStation',
+    secret: 'somerandomstuff',
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -47,10 +38,7 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err.message);
-        console.error('Ensure the MySQL credentials are correct and the MySQL service is running.');
-        process.exit(1); 
-    } else {
-        console.log('Connected to database');
+        process.exit(1);
     }
 });
 
@@ -60,14 +48,13 @@ global.db = db;
 app.locals.siteData = { siteName: "Sec Co - Ortvin M Portfolio project" };
 
 // Load the route handlers
-const mainRoutes = require("./routes/main");
+const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
 
-// Load the route handlers for /users
 const usersRoutes = require('./routes/users');
 app.use('/users', usersRoutes);
 
-// Load the route handlers for /projects
+// Ensure this is declared only once
 const projectsRoutes = require('./routes/projects');
 app.use('/projects', projectsRoutes);
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,80 +1,49 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
-})
+router.get('/', async function (req, res, next) {
+    const username = "OrtvinM"; // GitHub username
 
-router.post('/search', function (req, res, next) {
-    const query = req.sanitize(req.body.query); 
-
-    const searchQuery = `
-        SELECT * FROM projects WHERE projectName LIKE ?
+    // Query local database for projects and associated user names
+    const sqlquery = `
+        SELECT 
+            projects.projectName, 
+            projects.GitLink, 
+            CONCAT(users.firstName, ' ', users.lastName) AS connectedUser 
+        FROM projects
+        LEFT JOIN views ON projects.idProjects = views.projectID
+        LEFT JOIN users ON views.userID = users.idusers
     `;
 
-    db.query(searchQuery, [`%${query}%`], function (err, results) {
+    db.query(sqlquery, async (err, dbResults) => {
         if (err) {
-            console.error('Error querying database:', err);
-            return res.status(500).send('Error executing search');
+            return res.render("projects.ejs", {
+                dbResults: [],
+                githubResults: [],
+                error: "Error querying the database"
+            });
         }
 
-        res.render('searchResults.ejs', { results });
+        try {
+            // Fetch GitHub projects
+            const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+            const githubProjects = response.data;
+
+            res.render("projects.ejs", {
+                dbResults,
+                githubResults: githubProjects,
+                error: null
+            });
+        } catch (error) {
+            console.error("Error fetching GitHub projects:", error.message);
+            res.render("projects.ejs", {
+                dbResults,
+                githubResults: [],
+                error: "Failed to fetch GitHub projects"
+            });
+        }
     });
 });
 
-
-router.get('/search_result', function (req, res, next) {
-    // Search the database
-    let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.search_text + "%'" // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("list.ejs", {availableBooks:result})
-     }) 
-})
-
-
-router.get('/list', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books" // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("list.ejs", {availableBooks:result})
-     })
-})
-
-router.get('/addbook', function (req, res, next) {
-    res.render('addbook.ejs')
-})
-
-router.post('/bookadded', function (req, res, next) {
-    // saving data in database
-    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
-    // execute sql query
-    let newrecord = [req.body.name, req.body.price]
-    db.query(sqlquery, newrecord, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        else
-            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
-    })
-}) 
-
-router.get('/bargainbooks', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books WHERE price < 20"
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("bargains.ejs", {availableBooks:result})
-    })
-}) 
-
-
-// Export the router object so index.js can access it
-module.exports = router
+module.exports = router;
